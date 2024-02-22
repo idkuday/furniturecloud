67f8018c0f0a78dcb79bbce0117eb4c5c0509293
Uday Raghavan <udayragh@gmail.com>
2024-02-21 18:59:05 -0600

    Some Home page changes

--- FurtitureCloud/src/app/app.component.ts
+++ FurtitureCloud/src/app/app.component.ts
@@ -10,10 +10,18 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export class AppComponent {
   items: any[];
+  categories: any[] = [["LivingRoom", "../assets/LivingRoom.jpg"], ["Lighting", "../assets/Lighting.jpg"],
+  ["Bedroom", "../assets/Bedroom.jpg"]];
+  routerPath(category: string) {  
+    // console.log("./" + category.toLowerCase);
+    
+    return "./" + category.toLowerCase();
+  }
   constructor(private productService: ProductService) {
    
     productService.getAllProducts();
     this.items = productService.products;
+
     
   }
 
