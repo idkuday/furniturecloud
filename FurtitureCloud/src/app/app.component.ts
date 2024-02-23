feac61aa1eb31facb53381089b7448ca62f6f545
Uday Raghavan <udayragh@gmail.com>
2024-02-22 18:30:43 -0600

    Some Home page changes

--- FurtitureCloud/src/app/app.component.ts
+++ FurtitureCloud/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from './product.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,34 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export class AppComponent {
   items: any[];
-  constructor(private productService: ProductService) {
-   
-    productService.getAllProducts();
-    this.items = productService.products;
+  
+  categories: any[] = [["LivingRoom", "../assets/LivingRoom.jpg"], ["Lighting", "../assets/Lighting.jpg"],
+  ["Bedroom", "../assets/Bedroom.jpg"]];
+  routerPath(category: string) {  
+    // console.log("./" + category.toLowerCase);
     
+    return "./" + category.toLowerCase();
+  }
+  constructor(private productService: ProductService, private userService:UserService) {
+ 
+
+    // productService.getAllProducts()
+    userService.getAllUsers()
+    // this.items = productService.products;
+    this.items = userService.users;
+    console.log(this.items);
+    setTimeout(() => {
+      this.updateUser(this.items[0])
+    }, 5000);
+  }
+
+  updateUser(user:any) {
+    user.firstName = "Uday"
+    this.userService.updateUser(user).subscribe(d=>console.log(d))
+    user.firstName="abc"
+    this.userService.getAllUsers()
+    // this.items = productService.products;
+    this.items = this.userService.users;
   }
 
   title = 'FurtitureCloud';
