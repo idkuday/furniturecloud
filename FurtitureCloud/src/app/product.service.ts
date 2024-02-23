367800241eb72eb8192cfe57539ccd7691a50910
Uday Raghavan <udayragh@gmail.com>
2024-02-22 18:30:43 -0600

    Some Home page changes

--- FurtitureCloud/src/app/product.service.ts
+++ FurtitureCloud/src/app/product.service.ts
@@ -9,10 +9,13 @@ export class ProductService {
   url: string= 'http://localhost:8080/product/';
   products: any[] =[];
   constructor(private httpClient: HttpClient) { 
+    
   }
 
   getProduct(SKU: number): any {
-    return this.httpClient.get(this.url + 'get/' + SKU);
+    let product ;
+    this.httpClient.get(this.url + 'get/' + SKU).subscribe(d => product = d);
+    return product;
   }
 
   getAllProducts(category="none") {//Category based query else all products are queried
@@ -22,6 +25,19 @@ export class ProductService {
     else
       this.httpClient.get<any[]>(this.url + 'getAll/Category/'+category).subscribe(e => { this.products.push(...e) })
      
+  }
+  createproduct(product: any) {
+    let sku:any=0;
+    this.httpClient.post(this.url + "create", product).subscribe(d => sku = d);
+    this.products.push(this.getProduct(sku));
+    
+  }
+  updateproduct(product: any) {
+    return this.httpClient.put(this.url + "update/", product);
+    
+  }
+  deleteproduct(email:string) {
+    return this.httpClient.delete(this.url + "delete/" +email);
   }
 
 }
