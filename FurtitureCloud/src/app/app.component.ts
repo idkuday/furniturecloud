be0216edbf8f6e143ecec08754f6acff0e9582fc
Uday Raghavan <udayragh@gmail.com>
2024-02-26 02:09:14 -0600

    Home page header

--- FurtitureCloud/src/app/app.component.ts
+++ FurtitureCloud/src/app/app.component.ts
@@ -1,48 +1,49 @@
 import { Component } from '@angular/core';
-import { ProductService } from './product.service';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { UserService } from './user.service';
+// import { ProductService } from './product.service';
+// import { provideHttpClient, withFetch } from '@angular/common/http';
+// import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  items: any[];
-
-  categories: any[] = [
-    ['LivingRoom', '../assets/LivingRoom.jpg'],
-    ['Lighting', '../assets/Lighting.jpg'],
-    ['Bedroom', '../assets/Bedroom.jpg'],
-  ];
-  routerPath(category: string) {
-    // console.log("./" + category.toLowerCase);
-
-    return './' + category.toLowerCase();
+  addToCart() {
+    this.cartLength++;
   }
-  constructor(
-    private productService: ProductService,
-    private userService: UserService
-  ) {
-    // productService.getAllProducts()
-    userService.getAllUsers();
-    // this.items = productService.products;
-    this.items = userService.users;
-    console.log(this.items);
-    setTimeout(() => {
-      this.updateUser(this.items[0]);
-    }, 5000);
-  }
+  placeholder: string = 'Search';
+  cartLength: number = 0;
+  // items: any[];
 
-  updateUser(user: any) {
-    user.firstName = 'Uday';
-    this.userService.updateUser(user).subscribe((d) => console.log(d));
-    user.firstName = 'abc';
-    this.userService.getAllUsers();
-    // this.items = productService.products;
-    this.items = this.userService.users;
-  }
+  // categories: any[] = [["LivingRoom", "../assets/LivingRoom.jpg"], ["Lighting", "../assets/Lighting.jpg"],
+  // ["Bedroom", "../assets/Bedroom.jpg"]];
+  // routerPath(category: string) {
+  //   // console.log("./" + category.toLowerCase);
+
+  //   return "./" + category.toLowerCase();
+  // }
+  // constructor(private productService: ProductService, private userService:UserService) {
+
+  //   // productService.getAllProducts()
+  //   userService.getAllUsers()
+  //   // this.items = productService.products;
+  //   this.items = userService.users;
+  //   console.log(this.items);
+  //   setTimeout(() => {
+  //     this.updateUser(this.items[0])
+  //   }, 5000);
+  // }
+
+  // updateUser(user:any) {
+  //   user.firstName = "Uday"
+  //   this.userService.updateUser(user).subscribe(d=>console.log(d))
+  //   user.firstName="abc"
+  //   this.userService.getAllUsers()
+  //   // this.items = productService.products;
+  //   this.items = this.userService.users;
+  // }
 
   title = 'FurtitureCloud';
 }
--- FurtitureCloud/src/app/product.service.ts
+++ FurtitureCloud/src/app/product.service.ts
@@ -1,43 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
 
 export class ProductService {
-  url: string= 'http://localhost:8080/product/';
-  products: any[] =[];
-  constructor(private httpClient: HttpClient) { 
-    
-  }
+  url: string = 'http://localhost:8080/product/';
+  products: any[] = [];
+  constructor(private httpClient: HttpClient) {}
 
   getProduct(SKU: number): any {
-    let product ;
-    this.httpClient.get(this.url + 'get/' + SKU).subscribe(d => product = d);
+    let product;
+    this.httpClient
+      .get(this.url + 'get/' + SKU)
+      .subscribe((d) => (product = d));
     return product;
   }
 
-  getAllProducts(category="none") {//Category based query else all products are queried
+  getAllProducts(category = 'none') {
+    //Category based query else all products are queried
     // represents products
-    if(category==="none")
-      this.httpClient.get<any[]>(this.url + 'getAll/none/none').subscribe(e => { this.products.push(...e) })
+    if (category === 'none')
+      this.httpClient
+        .get<any[]>(this.url + 'getAll/none/none')
+        .subscribe((e) => {
+          this.products.push(...e);
+        });
     else
-      this.httpClient.get<any[]>(this.url + 'getAll/Category/'+category).subscribe(e => { this.products.push(...e) })
-     
+      this.httpClient
+        .get<any[]>(this.url + 'getAll/Category/' + category)
+        .subscribe((e) => {
+          this.products.push(...e);
+        });
   }
   createproduct(product: any) {
-    let sku:any=0;
-    this.httpClient.post(this.url + "create", product).subscribe(d => sku = d);
+    let sku: any = 0;
+    this.httpClient
+      .post(this.url + 'create', product)
+      .subscribe((d) => (sku = d));
     this.products.push(this.getProduct(sku));
-    
   }
   updateproduct(product: any) {
-    return this.httpClient.put(this.url + "update/", product);
-    
+    return this.httpClient.put(this.url + 'update/', product);
   }
-  deleteproduct(email:string) {
-    return this.httpClient.delete(this.url + "delete/" +email);
+  deleteproduct(email: string) {
+    return this.httpClient.delete(this.url + 'delete/' + email);
   }
-
 }
