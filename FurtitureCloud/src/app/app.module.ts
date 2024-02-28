abd3d2e36785cf666b8356d3a30bc783fcfd3498
vkalahas <vkalahas@ucsd.edu>
2024-02-28 14:10:54 -0800

    cleaned up a lil

--- FurtitureCloud/src/app/app.module.ts
+++ FurtitureCloud/src/app/app.module.ts
@@ -3,19 +3,11 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withFetch,
-} from '@angular/common/http';
+
 import {
   HttpClientModule,
   provideHttpClient,
@@ -25,8 +17,6 @@ import { LampsComponent } from './lamps/lamps.component';
 import { LightingComponent } from './lighting/lighting.component';
 import { BedroomComponent } from './bedroom/bedroom.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
@@ -48,11 +38,8 @@ import { FooterComponent } from './footer/footer.component';
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, MaterialModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MaterialModule],
-  providers: [provideClientHydration(), provideAnimationsAsync()],
-  bootstrap: [AppComponent],
 })
-export class AppModule {
+
 export class AppModule {
   constructor() {
     provideHttpClient(withFetch());
