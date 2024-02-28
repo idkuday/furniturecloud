ee213f732d93d797a663f5d022dcf7b1c096ab82
vkalahas <vkalahas@ucsd.edu>
2024-02-28 14:00:45 -0800

    cart component

--- FurtitureCloud/src/app/app.module.ts
+++ FurtitureCloud/src/app/app.module.ts
@@ -3,10 +3,7 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,21 +13,16 @@ import {
   provideHttpClient,
   withFetch,
 } from '@angular/common/http';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withFetch,
-} from '@angular/common/http';
+
 import { LampsComponent } from './lamps/lamps.component';
 import { LightingComponent } from './lighting/lighting.component';
 import { BedroomComponent } from './bedroom/bedroom.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material/material.module';
-import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MaterialModule } from './material/material.module';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -40,19 +32,15 @@ import { FooterComponent } from './footer/footer.component';
     BedroomComponent,
     CategoriesComponent,
     HomeComponent,
-    CategoriesComponent,
-    HomeComponent,
+    CartComponent,
     HeaderComponent,
     FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, MaterialModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MaterialModule],
-  providers: [provideClientHydration(), provideAnimationsAsync()],
-  bootstrap: [AppComponent],
 })
-export class AppModule {
+
 export class AppModule {
   constructor() {
     provideHttpClient(withFetch());
--- FurtitureCloud/src/app/material/material.module.ts
+++ FurtitureCloud/src/app/material/material.module.ts
@@ -6,6 +6,14 @@ import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 // import { MatDividerModule } from '@angular/material/divider';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 const Materials = [
   MatToolbarModule,
@@ -14,6 +22,16 @@ const Materials = [
   MatIconButton,
   MatIconModule,
   MatBadgeModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  FormsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDividerModule,
 ];
 
 @NgModule({
