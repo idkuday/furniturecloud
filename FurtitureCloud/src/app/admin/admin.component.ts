5fc770116328f9360dae16e534b143204af87d71
vkalahas <vkalahas@ucsd.edu>
2024-03-04 15:44:51 -0800

    admin with furniture info

--- FurtitureCloud/src/app/admin/admin.component.ts
+++ FurtitureCloud/src/app/admin/admin.component.ts
@@ -2,27 +2,28 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface PeriodicElement {
+export interface Product {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  sku: number;
+  descr: string;
+  stock: number;
+  category: string;
   price: number;
 }
 
 
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Sofa', weight: 1, symbol: 'H', price: 1},
-  {position: 2, name: 'Sofa2', weight: 4, symbol: 'He', price: 2},
-  {position: 3, name: 'Sofa3', weight: 6, symbol: 'Li', price: 3},
-  {position: 4, name: 'Sofa4', weight: 9, symbol: 'Be', price: 4},
-  {position: 5, name: 'Sofa5', weight: 10, symbol: 'B', price: 5},
-  {position: 6, name: 'Sofa6', weight: 12, symbol: 'C', price: 6},
-  {position: 7, name: 'Lamp1', weight: 14, symbol: 'N', price: 7},
-  {position: 8, name: 'Lamp2', weight: 15, symbol: 'O', price: 8},
-  {position: 9, name: 'Lamp3', weight: 18, symbol: 'F', price: 9},
-  {position: 10, name: 'Lamp4', weight: 20, symbol: 'Ne', price: 10},
+const ELEMENT_DATA: Product[] = [
+  {sku: 1, name: 'Sofa', descr: 'aaa', stock: 1, category: 'Room', price: 1},
+  {sku: 2, name: 'Sofa2', descr: 'aaa',stock: 4, category: 'Room', price: 2},
+  {sku: 3, name: 'Sofa3', descr: 'aaa',stock: 6, category: 'Room', price: 3},
+  {sku: 4, name: 'Sofa4', descr: 'aaa',stock: 9, category: 'Room', price: 4},
+  {sku: 5, name: 'Sofa5', descr: 'aaa',stock: 10, category: 'Room', price: 5},
+  {sku: 6, name: 'Sofa6', descr: 'aaa',stock: 12, category: 'Room', price: 6},
+  {sku: 7, name: 'Lamp1', descr: 'aaa',stock: 14, category: 'Lighting', price: 7},
+  {sku: 8, name: 'Lamp2', descr: 'aaa',stock: 15, category: 'Lighting', price: 8},
+  {sku: 9, name: 'Lamp3', descr: 'aaa',stock: 18, category: 'Lighting', price: 9},
+  {sku: 10, name: 'Lamp4', descr: 'aaa',stock: 20, category: 'Lighting', price: 10},
 ];
 
 @Component({
@@ -31,10 +32,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrl: './admin.component.css',
 })
 export class AdminComponent {
-  displayedColumns: string[] = ['select', 'position', 'name', 'stock', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  dataSource1 = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['select', 'sku', 'name', 'descr', 'stock', 'category', 'price'];
+  dataSource = new MatTableDataSource<Product>(ELEMENT_DATA);
+  dataSource1 = new MatTableDataSource<Product>(ELEMENT_DATA);
+  selection = new SelectionModel<Product>(true, []);
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -54,11 +55,11 @@ export class AdminComponent {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
+  checkboxLabel(row?: Product): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.sku + 1}`;
   }
 }
 
